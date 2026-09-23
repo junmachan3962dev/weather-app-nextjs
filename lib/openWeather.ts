5da6c01937ad66fecd59976fb465cacd3b0f5d62
machan <junmachan3962.dev@gmail.com>
2026-09-24 07:53:13 +0900

    feat: OpenWeather APIの共通モジュール（lib/openWeather.ts）を作成

--- lib/openWeather.ts
+++ lib/openWeather.ts
@@ -18,7 +18,7 @@ async function fetchOpenWeather(url: string) {
 //1. 都市名から緯度・経度を取得
 export async function getCoordinatesByCity(city: string) {
     const apiKey = process.env.OPENWEATHER_API_KEY;
-
+    // [encodeURIComponent]によりcity(都市名)が日本語でも正しく受け取ることができる
     const url = `https://api.openweathermap.org/geo/1.0/direct?q=${encodeURIComponent(city)}&limit=1&appid=${apiKey}`;
     const data = await fetchOpenWeather(url);
 
@@ -41,9 +41,18 @@ export async function fetchWeatherData(lat: number, lon: number) {
     return await fetchOpenWeather(url);
 }
 
-// --- UIから都市名を受け取ってAPIから天気情報を取得しUIにデータを返すための関数 ---
+// --- APIでの処理を指示し必要なデータを取得するための司令塔の関数 ---
 export async function getWeatherByCity(city: string) {
   const { lat, lon, name } = await getCoordinatesByCity(city);
   const weatherData = await fetchWeatherData(lat, lon);
   return { cityName: name, weatherData };
 }
+
+/* 
+=== 処理の流れ ===
+1. getCoordinatesByCity()で該当する都市の緯度・経度を取得する（ジオコーディング）
+2. 取得した緯度・経度をfetchWeatherData()で受け取り
+   fetchOpenWeather()に天気データ取得のためのurlを送る 
+3. fetchOpenWeather()がAPIからデータを受け取りデータを返す 
+＊ getWeatherByCity()はAPIでの処理の流れを指揮する司令塔の役割
+*/
